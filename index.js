ecfbaa4e58c64ed6f350201eb1b455f58f6ebb31
Luciano Uchoa <103795412+luchoa54@users.noreply.github.com>
2024-03-05 19:38:44 +0000

    fix: delete function

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ require('dotenv').config()
 const express = require('express')
 const { MongoClient, ObjectID } = require('mongodb')
 
-const dbURL = process.env.DATABASE_URL
-const dbName = process.env.DATABASE_NAME
+class CRUDModel {
+    constructor(collection) {
+        this.collection = collection
+    }
+
+    async getAllItems() {
+        return await this.collection.find().toArray()
+    }
+
+    async getItemById(id) {
+        return await this.collection.findOne({ _id: new ObjectID(id) })
+    }
+
+    async addItem(item) {
+        try {
+            const result = await this.collection.insertOne(item)
+            return "Item adicionado com sucesso!"
+        } catch (error) {
+            console.log("Erro:", error)
+            return "Ocorreu uma falha ao adicionar o item!"
+        }
+    }
+
+    async updateItemById(id, newItem) {
+        await this.collection.updateOne({ _id: new ObjectID(id) }, { $set: newItem })
+        return "Item atualizado com sucesso!"
+    }
+
+    async deleteItemById(id) {
+        await this.collection.deleteOne({ _id: new ObjectID(id) })
+        return "Item deletado com sucesso!"
+    }
+}
 
 async function main() {
-
-    const client = new MongoClient(dbURL)
+    const client = new MongoClient(process.env.DATABASE_URL)
 
     console.log("Conectando ao banco de dados...")
     await client.connect()
@@ -16,62 +46,50 @@ async function main() {
     const app = express()
     app.use(express.json())
 
-    const db = client.db(dbName)
+    const db = client.db(process.env.DATABASE_NAME)
     const collection = db.collection('posts')
+    const crudModel = new CRUDModel(collection)
+
+    app.use((req, res, next) => {
+        res.setHeader('Content-Type', 'application/json');
+        next();
+    });
 
     app.get('/', function (req, res) {
-        res.send(dbName)
+        res.send("Olá mundo!")
     })
 
     app.get('/item', async function (req, res) {
-        const items = await collection.find().toArray()
-
+        const items = await crudModel.getAllItems()
         res.send(items)
     })
 
     app.get('/item/:id', async function (req, res) {
         const id = req.params.id
-
-        const item = await collection.findOne({
-            _id: new ObjectId(id)
-        })
-
+        const item = await crudModel.getItemById(id)
         res.send(item)
     })
 
     app.post('/item', async function (req, res) {
-        try {
-            const nome = req.body.nome
-
-            const result = await collection.insertOne({ nome })
-
-            res.send("Item adicionado com sucesso!")
-        } catch (error) {
-            console.log("Erro")
-            res.send("Ocorreu uma falha ao adicionar o item!")
-        }
+        const item = req.body
+        const resultMessage = await crudModel.addItem(item)
+        res.send(resultMessage)
     })
 
     app.put('/item/:id', async function (req, res) {
         const id = req.params.id
-
-        const novoItem = req.body
-
-        collection.updateOne(
-            { _id: new ObjectId(id) },
-            { $set: novoItem }
-        )
-
-        res.send("Item atualizado com sucesso!")
+        const newItem = req.body
+        const resultMessage = await crudModel.updateItemById(id, newItem)
+        res.send(resultMessage)
     })
 
     app.delete('/item/:id', async function (req, res) {
         const id = req.params.id
-
-        await collection.deleteOne({ _id: new ObjectId(id) })
-
+    
+        await collection.deleteOne( {_id: new ObjectId(id)})
+    
         res.send("Item deletado com sucesso!")
-    })
+      })
 
     app.listen(3000)
 }
