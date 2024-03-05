4c966d5cb1c32a07fe774f2f2392b9334f3d3664
Luciano Uchoa <103795412+luchoa54@users.noreply.github.com>
2024-03-05 17:45:59 +0000

    feat: CRUD

--- index.js
+++ index.js
@@ -1,8 +1,79 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const { MongoClient, ObjectID } = require('mongodb')
 
-app.get('/', function (req, res) {
-    res.send('Olá mundo')
-})
+const dbURL = process.env.DATABASE_URL
+const dbName = process.env.DATABASE_NAME
 
-app.listen(3000)
+async function main() {
+
+    const client = new MongoClient(dbURL)
+
+    console.log("Conectando ao banco de dados...")
+    await client.connect()
+    console.log("Banco de dados conectado com sucesso!")
+
+    const app = express()
+    app.use(express.json())
+
+    const db = client.db(dbName)
+    const collection = db.collection('posts')
+
+    app.get('/', function (req, res) {
+        res.send(dbName)
+    })
+
+    app.get('/item', async function (req, res) {
+        const items = await collection.find().toArray()
+
+        res.send(items)
+    })
+
+    app.get('/item/:id', async function (req, res) {
+        const id = req.params.id
+
+        const item = await collection.findOne({
+            _id: new ObjectId(id)
+        })
+
+        res.send(item)
+    })
+
+    app.post('/item', async function (req, res) {
+        try {
+            const nome = req.body.nome
+
+            const result = await collection.insertOne({ nome })
+
+            res.send("Item adicionado com sucesso!")
+        } catch (error) {
+            console.log("Erro")
+            res.send("Ocorreu uma falha ao adicionar o item!")
+        }
+    })
+
+    app.put('/item/:id', async function (req, res) {
+        const id = req.params.id
+
+        const novoItem = req.body
+
+        collection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: novoItem }
+        )
+
+        res.send("Item atualizado com sucesso!")
+    })
+
+    app.delete('/item/:id', async function (req, res) {
+        const id = req.params.id
+
+        await collection.deleteOne({ _id: new ObjectId(id) })
+
+        res.send("Item deletado com sucesso!")
+    })
+
+    app.listen(3000)
+}
+
+main()
